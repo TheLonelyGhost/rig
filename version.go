2cc91ab0034131f132c96aac334900276424a406
David Alexander <opensource@thelonelyghost.com>
2023-11-28 00:19:06 -0500

    Fixes version string reporting

--- version.go
+++ version.go
@@ -4,24 +4,18 @@ import "fmt"
 
 var (
 	Version    string = "1.0.0"
+	TreeState  string = "dirty"
 	Commit     string
 	CommitDate string
-	TreeState  string
 )
 
 func VersionString() (out string) {
-	out = fmt.Sprintf("rig v%s", Version)
-	if Commit == "" {
-		return
-	} else {
-		out += fmt.Sprintf("-%s", Commit)
+	out = fmt.Sprintf("rig %s", Version)
+	if Commit != "" {
+		out += fmt.Sprintf(" %s", Commit)
 	}
-
 	if TreeState != "" {
-		out += fmt.Sprintf(" %s", TreeState)
-	}
-	if CommitDate != "" {
-		out += fmt.Sprintf(" (%s)", CommitDate)
+		out += fmt.Sprintf(" (%s)", TreeState)
 	}
 
 	return
